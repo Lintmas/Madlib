e56126b12a417211c1f5cd2e088b83fb7b6e88f1
Genius <lonewolffirefox@gmail.com>
2023-04-11 03:21:07 -0700

    version One.01

---
+++ Madlib.py
@@ -0,0 +1,36 @@
+import time
+
+
+
+print("::::::::::welcome to this Madlib::::::::::")
+
+time.sleep(1)
+print("::::::::::loading Madlib::::::::::")
+time.sleep(2)
+noun = input("Name an animal:::::")
+noun2 =input("Name another animal:::::")
+noun3=input("Name an place:::::")
+adjective=input("Give me an adjective:::::")
+adjective2=input("Give me a adjective:::::")
+noun4 =input("Give me a plural:::::")
+noun5 =input("Give me a plural:::::")
+noun6=input("Give me a plural:::::")
+
+
+print(f"The {noun} and ")
+print(f"the {noun2} went ")
+print(f"to {noun3} In ")
+print(f"a {adjective} {adjective2} boat, They took")
+print(f"some {noun4}, and plenty ")
+print(f"of {noun5}, Wrapped up ")
+print(f"in a five-pound note. The {noun}looked up to the ")
+print(f"{noun6}O lovely {noun2}! O")
+print(f"{noun2}, my love, What a ")
+print(f"beautiful {noun2} you are,") 
+print(f"You are, You are! What a beautiful {noun2} you are!")
+output = print("this is a patch")
+
+
+amendd
+
+print(output)
